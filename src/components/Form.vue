51fb2e3f7de2f341b0f48d246d5e7f8e53cbc979
aaa006bd <aaa006bd@gmail.com>
2020-06-19 19:16:14 +0200

    dynamic result added for card

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,213 +1,191 @@
 <template>
-    <v-app>
-        <div class="form-component">
-            <p class="date-title">Today : <b class="date-format">{{date}}</b></p>
-            <v-form v-model="valid" @submit="postData" action="#">
-                <v-container>
-                    <v-row>
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="dbt_1"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Dry Bulb Temp(previous)"
-                                    required
-                            ></v-text-field>
-                        </v-col>
+  <v-app>
+    <div class="form-component">
+      <p class="date-title">
+        Today :
+        <b class="date-format">{{date}}</b>
+      </p>
+      <v-form v-model="valid" @submit="postData" action="#">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="dbt_1"
+                :rules="nameRules"
+                :counter="10"
+                label="Dry Bulb Temp(previous)"
+                required
+              ></v-text-field>
+            </v-col>
 
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="dbt_2"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Dry Bulb Temp"
-                                    required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="wbt_1"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Wet Bulb Temp(previous)"
-                                    required
-                            ></v-text-field>
-                        </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="dbt_2"
+                :rules="nameRules"
+                :counter="10"
+                label="Dry Bulb Temp"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="wbt_1"
+                :rules="nameRules"
+                :counter="10"
+                label="Wet Bulb Temp(previous)"
+                required
+              ></v-text-field>
+            </v-col>
 
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="wbt_2"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Wet Bulb Temp"
-                                    required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="rh_1"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Relative Humidity(previous)"
-                                    required
-                            ></v-text-field>
-                        </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="wbt_2"
+                :rules="nameRules"
+                :counter="10"
+                label="Wet Bulb Temp"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="rh_1"
+                :rules="nameRules"
+                :counter="10"
+                label="Relative Humidity(previous)"
+                required
+              ></v-text-field>
+            </v-col>
 
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="rh_2"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Relative Humidity"
-                                    required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="pre_1"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Pressure(previous)"
-                                    required
-                            ></v-text-field>
-                        </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="rh_2"
+                :rules="nameRules"
+                :counter="10"
+                label="Relative Humidity"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="pre_1"
+                :rules="nameRules"
+                :counter="10"
+                label="Pressure(previous)"
+                required
+              ></v-text-field>
+            </v-col>
 
-                        <v-col
-                                cols="12"
-                                md="6"
-                        >
-                            <v-text-field
-                                    v-model="pre_2"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Pressure"
-                                    required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <v-col class="text-center">
-                    <div class="my-2 pink--text">
-                        <v-btn type="submit" large color="primary" v-on:click="onClickPrediction">Prediction</v-btn>
-                    </div>
-                </v-col>
-            </v-form>
-            <Result  v-show="response"></Result>
-        </div>
-    </v-app>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="pre_2"
+                :rules="nameRules"
+                :counter="10"
+                label="Pressure"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-col class="text-center">
+          <div class="my-2 pink--text">
+            <v-btn type="submit" large color="primary" v-on:click="onClickPrediction">Prediction</v-btn>
+          </div>
+        </v-col>
+      </v-form>
+      <Result v-if="response.ok==true" :res="response"></Result>
+    </div>
+  </v-app>
 </template>
 
 <script>
-    import Result from "./Result";
-    import { format ,getHours, getMonth, getDate} from 'date-fns'
-    let postData = function (event){
-        console.log(event);
-        let data = {
-            "time":{
-                "month":getMonth(new Date(), 'MM'),
-                "time":getHours(new Date(), 'dd'),
-                "date":getDate(new Date(), 'dd')
-            },
-            "features":[
-                {
-                    "dew_temp":[Number(this.dbt_1),Number(this.dbt_2)]
-                },
-                {
-                    "dry_temp":[Number(this.wbt_1),Number(this.wbt_2)]
-                },
-                {
-                    "relative_humidity":[Number(this.rh_1),Number(this.rh_2)]
-                },
-                {
-                    "pressure":[Number(this.pre_1),Number(this.pre_2)]
-                }
-            ]
-        };
-        console.log(data);
-        return data;
-    };
-    export default {
-        name: "Form",
-        components: {Result},
-        props: {
-            response : Boolean,
-        },
-        data: () => ({
-            valid: false,
-            show: false,
-            dbt_1: '',
-            dbt_2: '',
-            wbt_1: '',
-            wbt_2: '',
-            rh_1: '',
-            rh_2: '',
-            pre_1: '',
-            pre_2: '',
-            date : format(new Date(), 'MM/dd/yyyy'),
-            nameRules: [
-                v => !!v || 'Field is required',
-                v => v.length <= 5 || 'Field must be less than 5 characters',
-            ],
-        }),
-        methods: {
-            postData,
-            onClickPrediction(e){
-                e.preventDefault();
-                this.$emit('clicked', this.postData());
-            }
-        }
+import Result from "./Result";
+import { format, getHours, getMonth, getDate } from "date-fns";
+let postData = function(event) {
+  console.log(event);
+  let data = {
+    time: {
+      month: getMonth(new Date(), "MM"),
+      time: getHours(new Date(), "dd"),
+      date: getDate(new Date(), "dd")
+    },
+    features: [
+      {
+        dew_temp: [Number(this.dbt_1), Number(this.dbt_2)]
+      },
+      {
+        dry_temp: [Number(this.wbt_1), Number(this.wbt_2)]
+      },
+      {
+        relative_humidity: [Number(this.rh_1), Number(this.rh_2)]
+      },
+      {
+        pressure: [Number(this.pre_1), Number(this.pre_2)]
+      }
+    ]
+  };
+  console.log(data);
+  return data;
+};
+export default {
+  name: "Form",
+  components: { Result },
+  props: {
+    response: Object
+  },
+  data: () => ({
+    valid: false,
+    show: false,
+    dbt_1: "",
+    dbt_2: "",
+    wbt_1: "",
+    wbt_2: "",
+    rh_1: "",
+    rh_2: "",
+    pre_1: "",
+    pre_2: "",
+    date: format(new Date(), "MM/dd/yyyy"),
+    nameRules: [
+      v => !!v || "Field is required",
+      v => v.length <= 5 || "Field must be less than 5 characters"
+    ]
+  }),
+  methods: {
+    postData,
+    onClickPrediction(e) {
+      e.preventDefault();
+      this.$emit("clicked", this.postData());
     }
+  }
+};
 </script>
 
 <style scoped>
-    .form-component{
-        width: 700px;
-        margin: auto;
-        padding: 60px 30px;
-        background: #fcfffa;
-        border-radius: 5px;
-    }
-    .form-component .form-custom{
-        width: 100%;
-    }
-    .date-title{
-        font-size: 30px;
-        margin-right: 10px;
-        background-color: #1976d2;
-        color: white;
-        padding: 5px;
-        border-radius: 5px;
-        margin-bottom: 30px;
-    }
-    .date-format{
-        font-size: 30px;
-    }
-
+.form-component {
+  width: 700px;
+  margin: auto;
+  padding: 60px 30px;
+  background: #fcfffa;
+  border-radius: 5px;
+}
+.form-component .form-custom {
+  width: 100%;
+}
+.date-title {
+  font-size: 30px;
+  margin-right: 10px;
+  background-color: #1976d2;
+  color: white;
+  padding: 5px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+}
+.date-format {
+  font-size: 30px;
+}
 </style>
